f6032489e449b82b15c8252f5cbe0041033fc507
Andrej <andrejstanic3@gmail.com>
2026-07-08 18:05:51 +0200

    Add animation logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,38 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Vector2 moveInput;
+    private Vector2 lastMoveInput;
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
     }
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-    }
 
-
-    void FixedUpdate()
-    {
-        rb.linearVelocity = moveInput * moveSpeed;
+        if (moveInput == Vector2.zero)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", lastMoveInput.x);
+            animator.SetFloat("LastInputY", lastMoveInput.y);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+            lastMoveInput = moveInput;
+        }
     }
 }
