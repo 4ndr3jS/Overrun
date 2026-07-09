d44060713e4b6d30fddf332801d1c9d0ebddacff
Andrej <andrejstanic3@gmail.com>
2026-07-09 16:06:15 +0200

    Add sign for shop

--- Assets/Scripts/MapTransition.cs
+++ Assets/Scripts/MapTransition.cs
@@ -14,16 +14,43 @@ public class MapTransition : MonoBehaviour
     public CinemachineCamera cmCamera;
     public CinemachineConfiner2D confiner;
 
+    private Collider2D myCollider;
+    private bool ignoreTrigger = false;
+    private Transform ignoredPlayer;
+
+    private void Awake()
+    {
+        myCollider = GetComponent<Collider2D>(); ;
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(!other.CompareTag("Player"))
             return;
+
+        if (ignoreTrigger)
+            return;
+
         if(DoorA == null || DoorA.DoorB == null)
         {
             Debug.LogWarning("Something didn't link");
             return;
         }
+
         TeleportPlayer(other.transform);
+    }
+    
+    private void Update()
+    {
+        if (ignoreTrigger && ignoredPlayer != null && myCollider != null)
+        {
+            bool stillInside = myCollider.OverlapPoint(ignoredPlayer.position);
+            if (!stillInside)
+            {
+                ignoreTrigger = false;
+                ignoredPlayer = null;
+            }
+        }
     }
 
     public void TeleportPlayer(Transform player)
@@ -50,5 +77,13 @@ public class MapTransition : MonoBehaviour
             Vector3 delta = player.position - oldPosition;
             cmCamera.OnTargetObjectWarped(player, delta);
         }
+
+        DoorA.ArmIgnore(player);
+    }
+
+    private void ArmIgnore(Transform player)
+    {
+        ignoreTrigger = true;
+        ignoredPlayer = player;
     }
 }
