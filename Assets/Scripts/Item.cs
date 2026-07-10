8661db5003bde3179a675c59cc0bc8e0fcb06eef
Andrej <andrejstanic3@gmail.com>
2026-07-11 00:55:27 +0200

    Add item pickup UI and make it fade out

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
     public int ID;
+    public string Name;
+
+    public void PickUp()
+    {
+        Sprite itemIcon = GetComponent<Image>().sprite;
+        if(ItemPickerUIController.Instance != null)
+        {
+            ItemPickerUIController.Instance.ShowItemPickup(Name, itemIcon);
+        }
+    }
 }
---
+++ Assets/Scripts/ItemPickerUIController.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ItemPickerUIController : MonoBehaviour
+{
+    public static ItemPickerUIController Instance { get; private set; }
+
+    public GameObject popupPrefab;
+    public int maxPopups = 5;
+    public float popupDuration = 3f;
+
+    private readonly Queue<GameObject> activePopups = new();
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Multiple ItemPickerUIController instances detected! Destroying the extras");
+            Destroy(gameObject);
+        }
+    }
+
+    public void ShowItemPickup(string itemName, Sprite itemIcon)
+    {
+        GameObject newPopup = Instantiate(popupPrefab, transform);
+        newPopup.GetComponentInChildren<TMP_Text>().text = itemName;
+
+        Image itemImage = newPopup.transform.Find("Icon")?.GetComponent<Image>();
+        if (itemImage)
+        {
+            itemImage.sprite = itemIcon;
+        }
+
+        activePopups.Enqueue(newPopup);
+        if(activePopups.Count > maxPopups)
+        {
+            Destroy(activePopups.Dequeue());
+        }
+
+        StartCoroutine(FadeOutAndDestroy(newPopup));
+    }
+
+
+    private IEnumerator FadeOutAndDestroy(GameObject popup)
+    {
+        yield return new WaitForSeconds(popupDuration);
+        if (popup == null)
+            yield break;
+
+        CanvasGroup canvasGroup = popup.GetComponent<CanvasGroup>();
+        for (float timePassed = 0f; timePassed < 1f; timePassed += Time.deltaTime)
+        {
+            if(popup == null)
+                yield break;
+
+            canvasGroup.alpha = 1f - timePassed;
+            yield return null;
+        }
+
+        Destroy(popup);
+    }
+}
