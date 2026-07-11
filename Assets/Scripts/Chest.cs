61809fc9fd28ab00d2035c1d02bc47ffa36eb537
Andrej <andrejstanic3@gmail.com>
2026-07-11 18:40:31 +0200

    Added interactability and a chest

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Chest : MonoBehaviour, IInteractable
+{
+    public bool isOpened { get; private set; }
+    public string ChestID { get; private set; }
+    public GameObject itemPrefab;
+    public Sprite openedSprite;
+
+    void Start()
+    {
+        ChestID ??= PublicHelper.GenerateUniqueID(gameObject);
+    }
+
+
+    public bool CanInteract()
+    {
+        return !isOpened;
+    }
+
+    public void Interact()
+    {
+        if (!CanInteract())
+            return;
+        OpenChest();
+    }
+
+    private void OpenChest()
+    {
+        SetOpened(true);
+
+        if(itemPrefab != null)
+        {
+            GameObject droppedItem = Instantiate(itemPrefab, transform.position + Vector3.down, Quaternion.identity);
+            droppedItem.GetComponent<BounceEffect>().StartBounce();
+        }
+    }
+
+    public void SetOpened(bool opened)
+    {
+        if (isOpened = opened)
+        {
+            GetComponent<SpriteRenderer>().sprite = openedSprite;
+        }
+    }
+}
